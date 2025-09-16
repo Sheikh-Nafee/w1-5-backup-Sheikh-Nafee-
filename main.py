a15bc1324b5f052f7002e73b31160094605a15a3
Sheikh Tauheed Rahman Nafee <Sheikh.Nafee@student.lab.fi>
2025-09-16 12:35:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Calculate the area of a wall.") 
+
+# Ask for width 
+X = input("Enter the width in meters: ") 
+Width = float(X) 
+
+# Ask for height 
+Y = input("enter the height in meters: ") 
+Height = float(Y) 
+
+# Show entered values 
+print(f"Width is {Width} m and Height is {Height} m.") 
+
+# calculate area 
+Area = Width * Height 
+
+# Show result 
+print(f"The wall will be {Area} square meters")
